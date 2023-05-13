f95b65425d5bf5692cc5744ca40e3ba6c3d07d02
LeRocque <t.lerocque@gmail.com>
2023-05-13 15:39:41 -0400

    checkRequest-complete
    Co-authored-by: Ron Liu <ronliu@users.noreply.github.com>

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -3,11 +3,34 @@ const Event = require('../models/eventModel');
 const eventController = {};
 
 eventController.createEvent = async (req, res, next) => {
-  console.log(' At createEvent controller');
   const { name, dates, times } = req.body;
 
-  // TODO: check if request body is valid
-  console.log('Body deconstruct', name, dates, times);
+  function checkRequest() {
+    // check name
+    if (typeof name !== 'string') return false;
+    
+    // check dates
+    if (!Array.isArray(dates)) return false;
+    for (date of dates) {
+      if (typeof date !== 'string') return false;
+    }
+    
+    // check times
+    if (!Array.isArray(times) || times.length !== 2) return false;
+    for (time of times) {
+      if (typeof time !== 'number') return false
+    }
+    
+    return true;
+  }
+
+  if (!checkRequest()) {
+    return next({
+      log: `Error occurred in eventController.createEvent: Invalid request body`,
+      status: 400,
+      message: 'Request body wack'
+    });
+  }
 
   try {
     res.locals.newEvent = await Event.create({
@@ -23,6 +46,5 @@ eventController.createEvent = async (req, res, next) => {
     };
     return next(errObj);
   }
-}
-
+};
 module.exports = eventController;
