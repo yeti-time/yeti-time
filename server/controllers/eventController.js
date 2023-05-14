4dfc389032020f1b837c728b9be35774099398ed
LeRocque <t.lerocque@gmail.com>
2023-05-13 20:01:05 -0400

    getRequest-done

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -41,4 +41,28 @@ eventController.createEvent = async (req, res, next) => {
     });
   }
 };
+
+
+eventController.getEvent = async (req, res, next) => {
+  const {_id} = req.body;
+  console.log('id', _id);
+  if (typeof _id !== 'string') {
+    return next({
+      log: `Error occurred in eventController.getEvent: Invalid request body`,
+      status: 400,
+      message: 'Request body wack'
+    });
+  }
+  try {
+    res.locals.event = await Event.findOne({
+      _id: _id
+    });
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error occurred in eventController.getEvent: ${err.message}`
+    })
+  }
+}
+
 module.exports = eventController;
--- server/routes/event.js
+++ server/routes/event.js
@@ -9,4 +9,8 @@ router.post('/', eventController.createEvent, (req, res) => {
   return res.status(200).json(res.locals.newEvent);
 });
 
+router.get('/', eventController.getEvent, (req, res) => {
+  return res.status(200).json(res.locals.event);
+})
+
 module.exports = router;
