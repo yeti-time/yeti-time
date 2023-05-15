0eb456bff9ec289717be5d213ac2eb421ab6c1ee
Fabrizzio Quintanilla <fabrizzio7112@outlook.com>
2023-05-15 11:19:53 -0400

    finished the frontend now just adding extra features

---
+++ client/components/EventCreationPage.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import TimePicker from 'react-time-picker';
+import 'react-time-picker/dist/TimePicker.css';
+import { useNavigate } from "react-router-dom";
+import './Cal.css'
+
+function EventCreationPage() {
+  const [dates, setSelectedDates] = useState([]);
+  const [name, setName] = useState("")
+  const [startTime, setStartTime] = useState('12:00')
+  const [endTime, setEndTime] = useState('12:30')
+
+  const navigate = useNavigate()
+
+
+  const handleClick = (selectedDates) => {
+    setSelectedDates([...dates, selectedDates]);
+  }
+
+  const handleName = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleStartTime = (time) => {
+    setStartTime(time);
+  }
+  const handleEndTime = (time) => {
+    setEndTime(time);
+  }
+
+  const handleCreateEvent = () => {
+    const event = {
+      name: name,
+      dates: dates,
+      startTime: startTime,
+      endTime: endTime,
+    };
+
+    fetch('/api/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(event),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+            const id = data.id
+            navigate(`/api/events/${id}`)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    console.log('click')
+  };
+  
+
+  return (
+    <div className='Yeti'>
+        <div className='calenderSection'>  
+            <h1 className='text-center'> Yeti Time </h1>
+            <h2> What Days Would You Like to Meet?</h2>
+            <div className='calender-container'>
+            <Calendar onChange={handleClick} />
+            </div>
+            {/* <h2>Dates Selected: </h2>
+            {dates.map((dates, index) => (
+            <li key={index}>{dates.toLocaleDateString()}</li>
+            ))} */}
+        </div>
+        <div className='formSection'>
+        <div>
+            <label>Name of your Event </label>
+            <br></br>
+            <input className="eventName" type="text" placeholder='Event Name' value={name} onChange={handleName}/>
+        </div>
+
+        <div>
+            <TimePicker 
+                onChange={handleStartTime}
+                value={startTime}
+                clearIcon={null}
+                disableClock={true}
+                minuteInterval={30}
+            /> 
+            <h2> to </h2>
+            <TimePicker 
+                onChange={handleEndTime}
+                value={endTime}
+                clearIcon={null}
+                disableClock={true}
+            /> 
+        </div>
+
+        <div>
+            <button onClick={handleCreateEvent}>Create Event</button>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default EventCreationPage;
